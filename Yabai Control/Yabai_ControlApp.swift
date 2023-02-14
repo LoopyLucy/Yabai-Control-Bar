cbcb9dce1d6f98e512ff8b386f777a8b6fcb230a
ErinLucy <erinlucyfitton@outlook.com>
2023-02-14 19:40:11 +0000

    Rebuilt UI in SwiftUI

--- Yabai Control/Yabai_ControlApp.swift
+++ Yabai Control/Yabai_ControlApp.swift
@@ -9,9 +9,38 @@ import SwiftUI
 
 @main
 struct Yabai_ControlApp: App {
+    @State var tiled: Bool = true
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+           
+        MenuBarExtra("Yabai Control", image: tiled ? "Tiled" : "Loose") {
+            
+            Button("Start Yabai") {
+                tiled = true
+            }
+            
+            Button("Stop Yabai") {
+                tiled = false
+            }
+            
+            Button("Reset Yabai") {
+                
+            }
+            
+            Divider()
+            
+            Button("Open Settings") {
+                
+            }
+            
+            Divider()
+            
+            Button("Quit") {
+                NSApplication.shared.terminate(nil)
+            }
         }
     }
 }
