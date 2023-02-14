f738897d0a310d40df970b02c88e12c692cbab9b
ErinLucy <erinlucyfitton@outlook.com>
2023-02-14 19:06:44 +0000

    Initial Commit

---
+++ Yabai Control/Yabai_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yabai_ControlApp.swift
+//  Yabai Control
+//
+//  Created by Erin Fitton on 14/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Yabai_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
